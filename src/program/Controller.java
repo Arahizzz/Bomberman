584a89cf57fcf0abb7a0edf22098a627fc1de0b2
Yurii Polishchuk <yuriipolishchuk1@gmail.com>
2019-05-29 12:26:44 +0300

    Basic main menu layout.

--- src/program/Controller.java
+++ src/program/Controller.java
@@ -1,4 +1,19 @@
 package program;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    Button exit;
+
+    public void initialize() {
+        exit.setOnAction(this::exitButtonAction);
+    }
+
+    private void exitButtonAction(ActionEvent event) {
+        System.exit(0);
+    }
 }
--- src/program/Main.java
+++ src/program/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Bomberman");
         primaryStage.setScene(new Scene(root, 300, 275));
         ScreenController screenController = new ScreenController(primaryStage.getScene());
         screenController.addScreen("MainMenu", FXMLLoader.load(getClass().getResource("mainMenu.fxml")));
