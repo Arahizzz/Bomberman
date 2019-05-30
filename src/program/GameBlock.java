0e66d0d07c4d9aa9939fe1d60252dec140c6cc28
Yurii Polishchuk <yuriipolishchuk1@gmail.com>
2019-05-30 15:55:59 +0300

    Changed block hierarchy.

---
+++ src/program/GameBlock.java
@@ -0,0 +1,43 @@
+package program;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+abstract public class GameBlock extends Rectangle {
+    boolean breakable;
+    boolean walkAllowed;
+
+    public GameBlock(double x, double y, double width, double height, boolean breakable, boolean walkAllowed) {
+        super(x, y, width, height);
+        this.breakable = breakable;
+        this.walkAllowed = walkAllowed;
+    }
+
+    public boolean isBreakable() {
+        return breakable;
+    }
+
+    public boolean isWalkAllowed() {
+        return walkAllowed;
+    }
+}
+
+class StoneBrick extends GameBlock {
+
+    public StoneBrick(int x, int y, int width, int height){
+        super(x, y, width, height, false, false);
+        setStroke(Color.BLACK);
+        setFill(Color.LIGHTGRAY);
+    }
+}
+
+
+class RedBrick extends GameBlock {
+
+    public RedBrick(int x, int y, int width, int height) {
+        super(x, y, width, height, true, false);
+        setStroke(Color.BLACK);
+        setFill(Color.ORANGE);
+    }
+}
+
