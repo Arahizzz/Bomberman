fd2e2f70ae609070220b7a04cc9b5608900b830f
Yurii Polishchuk <yurasik081@gmail.com>
2019-05-31 23:14:38 +0300

    Centered map and added background.

--- src/program/SingleGame.java
+++ src/program/SingleGame.java
@@ -1,9 +1,16 @@
 package program;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -16,8 +23,12 @@ public class SingleGame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Single game");
+        VBox pane = new VBox();
         Group group = new Group();
-        primaryStage.setScene(new Scene(group,WIN_WIDTH,WIN_HEIGTH));
+        pane.setAlignment(Pos.CENTER);
+        pane.getChildren().add(group);
+        pane.setBackground(new Background(new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+        primaryStage.setScene(new Scene(pane, WIN_WIDTH, WIN_HEIGTH));
         GamePlayGround gamePlayGround = new GamePlayGround(group.getChildren(),WIN_WIDTH,WIN_HEIGTH);
         gamePlayGround.drawGrid();
         gamePlayGround.initPlayer();
