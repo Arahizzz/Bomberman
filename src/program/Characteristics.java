3ced0fae8160da2b8d185db2d778d4bba5981d1c
artikgak <artem.hak@ukma.edu.ua>
2019-06-06 22:39:25 +0300

    increse font on characteristics

--- src/program/Characteristics.java
+++ src/program/Characteristics.java
@@ -7,32 +7,34 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.text.Font;
 
-import java.text.DecimalFormat;
-
-import static java.lang.Math.PI;
 
 public class Characteristics extends GridPane {
 
     private static final int WIDTH = 200;
     private static final int HEIGHT = 200;
-
+    private Font font = new Font("",24);
 
     public Characteristics(Player player){
         Image imageHeart = new Image("Powerups\\Heart.png");
         Label heartL = new Label();
+        heartL.setFont(font);
         heartL.textProperty().bind(player.lifeProperty());
 
         Image imageFlame= new Image("Powerups\\FlamePowerup.png");
         Label flameL = new Label();
+        flameL.setFont(font);
         flameL.textProperty().bind(player.rangeProperty());
 
         Image imageBomb = new Image("Powerups\\BombPowerup.png");
         Label bombL = new Label();
+        bombL.setFont(font);
         bombL.textProperty().bind(player.maxCountProperty());
 
         Image imageSpeed = new Image("Powerups\\SpeedPowerup.png");
         Label speedL = new Label();
+        speedL.setFont(font);
         speedL.textProperty().bind(Bindings.format("%.3f", player.speedProperty()));
 
         this.getColumnConstraints().add(new ColumnConstraints(70));
