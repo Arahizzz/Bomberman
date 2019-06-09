320346e918fe53db44555ec1f6d81b8408c3be52
Yurii Polishchuk <yurasik081@gmail.com>
2019-06-10 02:04:36 +0300

    Refactor SingleGame

--- src/program/SingleGame.java
+++ src/program/SingleGame.java
@@ -13,34 +13,48 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class SingleGame {
-
+    HBox hPane;
+    VBox vPane;
+    Group group;
+    GamePlayGround gamePlayGround;
     Scene scene;
 
     public Scene start(double winWidth, double winHeigth) {
-        HBox hPane = new HBox();
-        VBox vPane = new VBox();
-        Group group = new Group();
-        hPane.setAlignment(Pos.CENTER);
-        vPane.getChildren().add(group);
-        hPane.setBackground(new Background(new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY)));
-        GamePlayGround gamePlayGround = new GamePlayGround(group.getChildren(), winWidth, winHeigth);
-        gamePlayGround.drawGrid();
+        setGameField(winWidth, winHeigth);
         gamePlayGround.initPlayer();
         scene = new Scene(hPane, winWidth, winHeigth);
         initListeners(gamePlayGround);
 
-        Characteristics characteristics = new Characteristics(gamePlayGround.getPlayer());
-        characteristics.setAlignment(Pos.CENTER);
-        hPane.getChildren().addAll(characteristics, vPane);
-
-        gamePlayGround.getPlayer().isAliveProperty().addListener(observable -> {
-            showEndScreen("You have lost.");
-        });
+        addCharacterisitcs(gamePlayGround.getPlayer());
+        addEndListeners(gamePlayGround.getPlayer());
         Exit.hasBeenCollectedProperty().addListener(observable -> {
             showEndScreen("You have won.");
         });
 
         return scene;
+    }
+
+    private void setGameField(double winWidth, double winHeigth) {
+        hPane = new HBox();
+        vPane = new VBox();
+        group = new Group();
+        hPane.setAlignment(Pos.CENTER);
+        vPane.getChildren().add(group);
+        hPane.setBackground(new Background(new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+        gamePlayGround = new GamePlayGround(group.getChildren(), winWidth, winHeigth);
+        gamePlayGround.drawGrid();
+    }
+
+    private void addCharacterisitcs(Player player) {
+        Characteristics characteristics = new Characteristics(player);
+        characteristics.setAlignment(Pos.CENTER);
+        hPane.getChildren().addAll(characteristics, vPane);
+    }
+
+    private void addEndListeners(Player player) {
+        player.isAliveProperty().addListener(observable -> {
+            showEndScreen("You have lost.");
+        });
     }
 
     public void showEndScreen(String text) {
